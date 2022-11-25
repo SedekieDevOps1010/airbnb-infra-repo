b43f4b60d77e2a9c04ecb19762f1ded681fc96c0
Sedekie Kamara <sedekie05@gmail.com>
2022-11-24 21:35:45 -0600

    created rds, 02

---
+++ rds.tf
@@ -0,0 +1,13 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydb"
+  username             = "sk"
+  password             = "08CloudEngineer%"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+}
